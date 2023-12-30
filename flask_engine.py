9f81ee5dbac411be01bda5e8930803ea622795bc
ekarkat <ekarkat@gmail.com>
2023-12-30 15:09:31 +0000

    add index

---
+++ flask_engine.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Flask Model"""
+
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+	return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
